6fc074380971350a3b285c8c7396c5bd6f7b631a
Xisheng Cai <xishengcai@XishengdeMacBook-Pro.local>
2020-04-13 14:00:23 +0800

    update

--- build.sh
+++ build.sh
@@ -1,46 +1,41 @@
 #!/bin/bash
 
-export GOPROXY=https://goproxy.io
+set -e
 
 BIN_FILE="cloud"
 IMAGE_NAME="cloud"
-VERSION="v1"
-CURRENT_DIRECTORY=${PWD##*/}
+VERSION="latest"
 
-echo "set build info"
-GIT_COMMIT=$(git rev-parse --short HEAD || echo "GitNotFound")
-BUILD_TIME=$(date +%FT%T%z)
+# echo "set build info"
+# GIT_COMMIT=$(git rev-parse --short HEAD || echo "GitNotFound")
+# BUILD_TIME=$(date +%FT%T%z)
 
-echo $GIT_COMMIT $BUILD_TIME
-LDFLAGS="-X main.GitCommit=${GIT_COMMIT} -X main.BuildTime=${BUILD_TIME}"
+# echo $GIT_COMMIT $BUILD_TIME
+# LDFLAGS="-X main.GitCommit=${GIT_COMMIT} -X main.BuildTime=${BUILD_TIME}"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "${LDFLAGS}" -o  ./bin/${BIN_FILE} ./main.go
+# CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "${LDFLAGS}" -o  ./bin/${BIN_FILE} ./main.go
 
-if [ $? -ne 0 ]; then
-    echo "build ERROR"
-    exit 1
-fi
-echo build success
+# if [ $? -ne 0 ]; then
+#     echo "build ERROR"
+#     exit 1
+# fi
+# echo build success
 
 cat <<EOF > Dockerfile
+############# builder c #############
+FROM golang:1.13.0 as builder
+
+WORKDIR /go/src/cloud
+COPY . .
+RUN GO111MODULE=off CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go install 
+
+
+############# cloud controller manager #############
 FROM alpine:latest
-
-MAINTAINER xishengcai <cc710917049@163.com>
-
-COPY ./bin/${BIN_FILE} /usr/local/bin
-COPY ./download /opt/download
-COPY ./template /opt/template
-COPY ./swaggerui /opt/swaggerui
-COPY ./conf   /opt/conf
-
-RUN chmod +x /usr/local/bin/${BIN_FILE}
-
-WORKDIR /opt
-
-EXPOSE 80
-
-CMD ["${BIN_FILE}"]
+COPY --from=builder /go/bin/cloud /cloud
+ENTRYPOINT  ["/cloud"]
 EOF
 
-docker build -t registry.cn-hongkong.aliyuncs.com/xisheng/${IMAGE_NAME}:${VERSION} ../${CURRENT_DIRECTORY}
-docker push registry.cn-hongkong.aliyuncs.com/xisheng/${IMAGE_NAME}:${VERSION}
+docker build -t xishengcai/${IMAGE_NAME}:${VERSION} ./
+docker push xishengcai/${IMAGE_NAME}:${VERSION}
+docker rmi xishengcai/${IMAGE_NAME}:${VERSION}
